fa5183c9554cb43bd355df9b53e40414c0b3a846
Caroline Viana <33549782+vianaclaus@users.noreply.github.com>
2019-01-07 00:08:12 -0200

    Lesson five
    Lesson five

---
+++ calculating_r2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def compute_r_squared(data, predictions):
+    # Write a function that, given two input numpy arrays, 'data', and 'predictions,'
+    # returns the coefficient of determination, R^2, for the model that produced 
+    # predictions.
+    # 
+    # Numpy has a couple of functions -- np.mean() and np.sum() --
+    # that you might find useful, but you don't have to use them.
+
+    # YOUR CODE GOES HERE
+    
+    r1 = np.sum((data-predictions)**2)
+    r2 = np.sum((data - np.mean(data))**2)
+    
+    r_squared = 1 - r1/r2
+    return r_squared
